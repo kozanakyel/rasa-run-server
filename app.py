ba7a31b2be4d0da6285b86ce4b68b946faebdb0e
kozanakyel <kozanakyel@gmail.com>
2023-03-06 14:40:06 +0300

    added spacy encore web

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ if __name__ == '__main__':
     #port = int(os.getenv("PORT"))
     #print(f'Port is {port}')
     port=5005
+    os.system('python -m spacy download en_core_web_md')
     os.system('rasa run --enable-api --cors \"*\" --port {}'.format(port))
     
     
