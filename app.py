93c0abb16d3510e6e02a93ca7753287f667bc432
kozanakyel <kozanakyel@gmail.com>
2023-03-06 16:45:01 +0300

    added app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import os
 
 if __name__ == '__main__':
-    #port = int(os.getenv("PORT"))
+    port = int(os.getenv("PORT"))
     #print(f'Port is {port}')
-    port=5005
-    os.system('python -m spacy download en_core_web_md')
+    #port=5005
+    #os.system('python -m spacy download en_core_web_md')
     os.system('rasa run --enable-api --cors \"*\" --port {}'.format(port))
     
     
