00d266db83390c64ca53a666af097432ba8cec68
kozanakyel <kozanakyel@gmail.com>
2023-03-06 14:35:08 +0300

    python version in netlify2233

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import os
 
 if __name__ == '__main__':
-    port = int(os.getenv("PORT"))
-    print(f'Port is {port}')
-    port=5000
+    #port = int(os.getenv("PORT"))
+    #print(f'Port is {port}')
+    port=5005
     os.system('rasa run --enable-api --cors \"*\" --port {}'.format(port))
     
     
