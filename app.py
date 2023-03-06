b116c091a3e70c52ef2ca7cf19c629a9f72f887b
kozanakyel <kozanakyel@gmail.com>
2023-03-06 19:18:39 +0300

    edited files for tarin22233

--- app.py
+++ app.py
@@ -5,6 +5,6 @@ if __name__ == '__main__':
     #print(f'Port is {port}')
     #port=5005
     #os.system('python -m spacy download en_core_web_md')
-    os.system('rasa telemetry disable && rm -rf models/* && rasa train && rasa run --enable-api --cors \"*\" --port {}'.format(port))
+    os.system('rasa telemetry disable && rasa run --enable-api --cors \"*\" --port {}'.format(port))
     
     
