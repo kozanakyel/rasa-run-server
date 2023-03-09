fab9e06532ba32573e5cf9a793cabbb01fb4bb3e
kozanakyel <kozanakyel@gmail.com>
2023-03-09 18:05:23 +0300

    edited for new models

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 import os
 
 if __name__ == '__main__':
-    port = int(os.getenv("PORT"))
+    #port = int(os.getenv("PORT"))
     #print(f'Port is {port}')
-    #port=5005
+    port=5005
     #os.system('python -m spacy download en_core_web_md')
     os.system('rasa run --enable-api --cors \"*\" --port {}'.format(port))
+    
+#rasa run --enable-api --cors "*"
     
     
